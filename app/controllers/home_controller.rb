e118c0e096de3ee8311094f3c2ac2fc0b4849a83
Dmelentsevich <spongemel@yandex.ru>
2017-11-16 20:11:48 +0300

    Started facebook authorization implementation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
 class HomeController < ApplicationController
-  def front
+  def index
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :email, presence: true
+  validates :password, presence: true, length: { in: 6..72, wrong_lenght: "Password lenght should be in 3-45 characters" }
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
