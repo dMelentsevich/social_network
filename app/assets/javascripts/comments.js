e60625802b3b713d157e34a11856038b392603f4
Danila Melentsevich <spongemel@yandex.ru>
2017-12-01 20:37:13 +0300

    Fixed hiding comments-section

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,6 +2,6 @@ $(function(){
   $('.comments-link').click(function(e){
     e.preventDefault();
     var postId = $(this).data('post-id');
-    $('.comments-section[data-post-id=' + postId + ']').fadeToggle();
+    $('.post[data-post-id=' + postId + '] .comments-section').fadeToggle();
   })
 });
